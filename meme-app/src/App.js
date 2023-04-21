bda15013c69d4a37f59b242e5de960bf2928186f
FKXHX <fk-hassan@hotmail.co.uk>
2023-04-21 15:52:33 +0100

    Plan Completed

--- meme-app/src/App.js
+++ meme-app/src/App.js
@@ -5,35 +5,43 @@ import Display from './components/Display';
 import { useState, useEffect } from 'react';
 
 
-// High level plan - 
+// High level plan: - 
 
 // Find and agree on an API ✅
 // Call the API and fetch the data and console.log it ✅
 
-// Plan and construct how our app is going to look like
-// - Header
-// - Meme image
-// - Meme text
-// - Input for text
-// - Button to generate a new meme
+// Plan and construct how our app is going to look like (screenshot taken) ✅
+// - Header ✅
+// - Create a display components that contains the meme image✅
+// - Meme text that shows up on bottom of the image ✅
+// - Input for text 
+// - Button to generate a new meme✅
 
 // Create our components
 // - App
+//     - Created a text box with no function yet for display✅
+//     - if we have time; create top + bottom text field inputs✅
+//     - function for button click✅
+//      - onClick api is called✅
+//     - make the meme appear in a default state/placeholder✅
+//      - random number between 1 - 100 generated and linked the id attribute inside json ✅
+//      - meme displayed in Display component✅
+
+// - Input
+//     - Allow input for text
 //     - State for text Input
 //      - handle change for user adding text
 
-//     - function for button click
-//      - onClick api is called
-//      - random number between 1 - 100 generated and linked the id attribute inside json 
-//      - meme displayed in Display component
-// - Input
 
+// - CSS for App
+// -Add background✅
+// -Chose font✅
+// -Align the meme image, header, input field and button accordingly✅
+// - Resize the button and input field
+// - Consider to add a border around the meme image
+// - Consider border around the input field
 
-// Then we need to NPM i afterwards ✅
-// Then write simple code using REACT to test if we can create and input text
-
-
-// API: "https://api.imgflip.com/get_memes"
+// API: "https://api.imgflip.com/get_memes"✅
 
 
 function App() {
